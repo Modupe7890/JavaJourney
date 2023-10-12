4346067c37cb902fabc245ca54a8c3a23390a62a
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-10-12 21:58:40 +0530

    Your message

---
+++ src/Chapter2/Exercise.java
@@ -0,0 +1,9 @@
+package Chapter2;
+
+public class Exercise{
+    public static void main(String[] args) {
+        int c;
+        int thisIsAVariable;
+        int q76354;
+    }
+}
---
+++ src/Chapter2/Exercise4.java
@@ -0,0 +1,11 @@
+package Chapter2;
+import java.util.Scanner;
+public class Exercise4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = 8;
+
+        if (number !=7);
+        System.out.println("The variable number is not equal to 7");
+    }
+}
