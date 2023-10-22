cb84a1ea389e3db1f6fcef8091806019e80f5525
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-10-22 17:15:04 +0530

    Your message

--- src/Chapter2/Exercise22.java
+++ src/Chapter2/Exercise22.java
@@ -8,17 +8,19 @@ public class Exercise22 {
 
         int number1;
         int number2;
-
+        System.out.println("Make sure the first number is greater than the second number to get accurate answer");
+        System.out.println("----------------------------------------------------------");
         System.out.print("Enter the first integer: ");
         number1 = input.nextInt();
         System.out.print("Enter the second integer: ");
         number2 = input.nextInt();
 
-        if (number2 % number1 >=1){
-            System.out.printf("The first number is a multiple of the second number %d%n", number2);
+
+        if (number1 % number2 >=1){
+            System.out.printf("The second number %d is not a multiple of the first number %d%n",number2, number1);
         }
-        if (number2 % number1 !=1){
-            System.out.printf("The first number is not a multiple of the second number %d%n", number2);
+        if (number1 % number2 ==0){
+            System.out.printf("The Second number %d is a multiple of the first number %d%n",number2, number1);
         }
     }
 }
