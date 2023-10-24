62022a134b20ea0578535a5eb28ffb8c92e4e2ae
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-10-24 17:02:11 +0530

    Your Message

---
+++ src/Chapter3/Account_Fig3_1.java
@@ -0,0 +1,18 @@
+package Chapter3;
+// Account class that contains a name instance vaviable
+// and method to set and get its value
+public class Account_Fig3_1 {
+    private String name; // instance variable
+
+    // method to set the name in the object
+
+    public void setName(String name) {
+        this.name = name; // store the name
+    }
+    // method to retrieve the name from the object
+    public  String getName() {
+        return name; // return value of name to caller
+
+        }
+    }
+
