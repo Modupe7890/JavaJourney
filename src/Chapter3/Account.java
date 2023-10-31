74110037b1b775ff4abd1df8332e2569d74f0eb7
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-10-31 07:29:36 +0530

    Your Message

--- src/Chapter3/Account.java
+++ src/Chapter3/Account.java
@@ -1,18 +1,25 @@
 package Chapter3;
-// Account class that contains a name instance variable
-// and method to set and get its value
 public class Account {
     private String name; // instance variable
-
-    // method to set the name in the object
-
+    private double balance;
+    public Account (String name, double balance) {
+        this.name = name;
+    if (balance >0.0)
+        this.balance = balance;
+    }
+    public void deposit(double depositAmount){
+        if (depositAmount>0.0)
+            balance = balance + depositAmount;
+    }
+    public double getBalance() {
+        return balance;
+    }
     public void setName(String name) {
-        this.name = name; // store the name
+        this.name = name;
     }
-    // method to retrieve the name from the object
-    public  String getName() {
-        return name; // return value of name to caller
+    public String getName() {
+        return name;
+    }
 
-        }
-    }
+}
 
--- src/Chapter3/AccountTest.java
+++ src/Chapter3/AccountTest.java
@@ -1,25 +1,39 @@
 package Chapter3;
-// Creating and manipulating an Account object
+import java.util.Arrays;
 import java.util.Scanner;
 public class AccountTest {
     public static void main(String[] args) {
-        // Create a scanner object to obtain input from the command window
+        Account account1 = new Account("Jane Green", 50.0);
+        Account account2 = new Account("John Blue", -7.35);
+
+        System.out.printf("%s balance: $%2.f%n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%2.f%n%n",
+                account2.getName(), account2.getBalance());
+
         Scanner input = new Scanner (System.in);
-        // create an Account and assign it to myAccount
 
-        Account myAccount = new Account();
-        // Display initial value of name (null)
-        System.out.printf("Initial name is: %s%n", myAccount.getName());
-        // prompt for and read me
-        System.out.println("Please enter the name:");
+        System.out.print("Enter deposit amount for account1: ");
+        double depositAmount = input.nextDouble();
+        System.out.printf("%nadding %2.f to account1 balance%n%n",
+                depositAmount);
+        account1.deposit(depositAmount);
 
-        String theName = input.nextLine(); // read a line of text
-        myAccount.setName(theName); // put theName in myAccount
-        System.out.println(); // output a blank line
+        System.out.printf("%s balance: $%2.f%n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%2.f%n",
+                account2.getName(), account2.getBalance());
 
-        // Displayed the name stored in object myAccount
-        System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+        System.out.print("Enter deposit amount for account2: ");
+        depositAmount = input.nextDouble();
+        System.out.printf("%nadding %2.f to account2 balance%n%n",
+                depositAmount);
+        account2.deposit(depositAmount);
 
+        System.out.printf("%s balance: $%2.f%n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%2.f%n",
+                account2.getName(), account2.getBalance());
 
     }
 }
