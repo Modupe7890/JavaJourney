df8ceeaa87d94683238750d03624728c4d547239
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-03 20:32:54 +0530

    Your Message

--- src/Chapter3/AccountTest.java
+++ src/Chapter3/AccountTest.java
@@ -5,33 +5,33 @@ public class AccountTest {
         Account account1 = new Account("Jane Green", 50.0);
         Account account2 = new Account("John Blue", -7.35);
 
-        System.out.printf("%s balance: $%2.f%n",
+        System.out.printf("%s balance: %n",
                 account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%2.f%n%n",
+        System.out.printf("%s balance: %n",
                 account2.getName(), account2.getBalance());
 
         Scanner input = new Scanner (System.in);
 
         System.out.print("Enter deposit amount for account1: ");
         double depositAmount = input.nextDouble();
-        System.out.printf("%nadding %2.f to account1 balance%n%n",
+        System.out.printf("%s adding  to account1 balance %s%n",
                 depositAmount);
         account1.deposit(depositAmount);
 
-        System.out.printf("%s balance: $%2.f%n",
+        System.out.printf("%s balance: %n",
                 account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%2.f%n",
+        System.out.printf("%s balance: %n",
                 account2.getName(), account2.getBalance());
 
         System.out.print("Enter deposit amount for account2: ");
         depositAmount = input.nextDouble();
-        System.out.printf("%nadding %2.f to account2 balance%n%n",
+        System.out.printf("%n adding  to account2 balance %s%n",
                 depositAmount);
         account2.deposit(depositAmount);
 
-        System.out.printf("%s balance: $%2.f%n",
+        System.out.printf("%s balance: %n",
                 account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%2.f%n",
+        System.out.printf("%d balance: %n",
                 account2.getName(), account2.getBalance());
 
     }
