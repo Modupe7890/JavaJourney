94a96944c8f9136cfd9c15c10cde2c6c634739ba
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-06 15:20:08 +0530

    your Message

--- src/Chapter3/Invoice.java
+++ src/Chapter3/Invoice.java
@@ -1,6 +1,22 @@
 package Chapter3;
 
 public class Invoice {
-    private int number;
+    private String invoiceNumber;
+    private String description;
     private int itemPurchased;
+    private double itemPrice;
+    public Invoice (String invoiceNumber, String description,int itemPurchased, double itemPrice){
+        this.invoiceNumber=invoiceNumber;
+        this.description=description;
+        this.itemPurchased=itemPurchased;
+        this.itemPrice=itemPrice;
+
+        public void setInvoiceNumber(String invoiceNumber){
+            this.invoiceNumber = invoiceNumber;
+        }
+
+        public String getInvoiceNumber() {
+            return invoiceNumber;
+        }
+
 }
