ace4881ccb14a5a930181e1bbadbae0cec255061
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-21 13:44:14 +0530

    Your Message

--- src/Chapter4/IdentifyErrorX.java
+++ src/Chapter4/IdentifyErrorX.java
@@ -1,13 +1,12 @@
 package Chapter4;
-import java.util.Scanner;
 public class IdentifyErrorX {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
         int x = 1, total = 0;
         while (x <= 10) {
             total += x;
             ++x;
+            System.out.printf("Total is %d%n", total);
         }
     }
 }
