720d9019e2cc7f02ba1f854dcb7463fd2811b5f6
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-23 05:13:17 +0530

    Your Message

--- src/Chapter4/CreditLimitTest.java
+++ src/Chapter4/CreditLimitTest.java
@@ -8,4 +8,13 @@ public class CreditLimitTest {
     private int totalCreditsApplied;
     private int creditLimit;
     private int newBalance;
+    public CreditLimit(int accountNumber, int beginingBalance, int totalItemsCharged, int totalCreditsApplied,
+                       int creditLimit) {
+
+        this.accountNumber = accountNumber;
+        this.beginingBalance = beginingBalance;
+        this.totalItemsCharged = totalItemsCharged;
+        this.totalCreditsApplied = totalCreditsApplied;
+        this.creditLimit = creditLimit;
+    }
 }
