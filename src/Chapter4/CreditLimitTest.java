f1f004f02477805ac2663faab4a18bbe079fc771
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-22 22:02:21 +0530

    Your Message

---
+++ src/Chapter4/CreditLimitTest.java
@@ -0,0 +1,11 @@
+package Chapter4;
+
+public class CreditLimitTest {
+
+    private int accountNumber;
+    private int beginingBalance;
+    private int totalItemsCharged;
+    private int totalCreditsApplied;
+    private int creditLimit;
+    private int newBalance;
+}
