542a837a358a4255510e6bfd7025b7c0acc26e93
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-22 21:57:24 +0530

    Your Message

---
+++ src/Chapter4/CreditLimit.java
@@ -0,0 +1,9 @@
+package Chapter4;
+
+public class CreditLimit {
+    private String name; // instance variable
+    private double balance;
+    public CreditLimit (String name, double balance) {
+        this.name = name;
+    }
+}
