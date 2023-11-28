ef8f97bfbc2195617ee1cba64e71e8e11516047c
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-28 16:28:05 +0530

    Your Message

---
+++ src/Chapter4/TwoLargestNum.java
@@ -0,0 +1,12 @@
+package Chapter4;
+import java.util.Scanner;
+public class TwoLargestNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = 1;
+        int counter = 0;
+        int firstLargest = 1;
+        int secondLargest =1;
+    }
+}
