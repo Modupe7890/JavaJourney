5cee79e1ca8ff972b4224d9430c0cfef08de9d5f
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-30 16:12:48 +0530

    Your Message

---
+++ src/Chapter4/Multiples.java
@@ -0,0 +1,14 @@
+package Chapter4;
+
+public class Multiples {
+    public static void main(String[] args) {
+
+        System.out.println("Multiples of 2");
+
+        int multiple =2;
+
+            for (int i =1; i < 1000; i++){
+            System.out.println(multiple * i);
+        }
+    }
+}
