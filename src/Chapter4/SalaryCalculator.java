c76dad954db16bcde29071ec128248bfc651d3fa
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-11-30 14:21:41 +0530

    Your Message

--- src/Chapter4/SalaryCalculator.java
+++ src/Chapter4/SalaryCalculator.java
@@ -5,16 +5,38 @@ public class SalaryCalculator {
 
         int hourWorked;
         int hourlyRate;
+        double extraHour;
+        double grossPay;
+        double CalculateGrossPay =0;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the number of hours you worked in a week: ");
+        System.out.println("Enter the first Employee's number of hours you worked in a week: ");
         hourWorked = input.nextInt();
 
-        System.out.println("Enter your hourly rate: ");
-        hourlyRate = input.nextInt();
+        System.out.println("Enter the second Employee's number of hours you worked in a week: ");
+        input.nextInt();
 
-        Double GrossPay =
+        System.out.println("Enter the third Employee's number of hours you worked in a week: ");
+        input.nextInt();
+
+        System.out.println("Enter first Employee's hourly rate: ");
+        hourlyRate = input.nextInt();
+        System.out.println("Enter second Employee's hourly rate: ");
+        input.nextInt();
+        System.out.println("Enter third Employee's hourly rate: ");
+        input.nextInt();
+
+        grossPay = hourWorked * hourlyRate;
+
+        if (hourWorked > 40){
+            extraHour = hourWorked - 40;
+
+            CalculateGrossPay = (grossPay + (extraHour / 2) + 40) * hourlyRate;
+        }
+        System.out.println("GrossPay for First Employee " +CalculateGrossPay);
+        System.out.println("GrossPay for Second Employee " +CalculateGrossPay);
+        System.out.println("GrossPay for Third Employee " +CalculateGrossPay);
 
     }
 
