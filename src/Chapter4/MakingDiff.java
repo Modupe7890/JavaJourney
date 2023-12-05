ccbb43e656a455022251eb36fbcd2016309dcbd7
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-12-05 13:37:50 +0530

    Your Message

---
+++ src/Chapter4/MakingDiff.java
@@ -0,0 +1,17 @@
+package Chapter4;
+
+public class MakingDiff {
+    public static void main(String[] args) {
+        double population = 800800000000.0;
+        double growthRate = 5.9;
+        double Calculate;
+
+
+        System.out.printf("%s%s%30s%n", "Year", "Anticipated World Pop", "Increase in World Pop");
+
+
+
+
+
+    }
+}
