cda9fc59f23a5dc85a7d7caa15237edb516be124
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-12-06 19:06:13 +0530

    Your Message

---
+++ src/Chapter5/LetterGrades.java
@@ -0,0 +1,15 @@
+package Chapter5;
+import java.util.Scanner;
+public class LetterGrades {
+    public static void main(String[] args) {
+        int total = 0;
+        int gradeCounter = 0;
+        int aCount = 0;
+        int bCount = 0;
+        int cCount = 0;
+        int dCount = 0;
+        int fCount = 0;
+
+        Scanner input = new Scanner(System.in);
+    }
+}
