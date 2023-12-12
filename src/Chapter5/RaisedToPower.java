a3007c34c849041d0a64f05ff5246c596673e9b8
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-12-12 13:51:27 +0530

    Your Message

--- src/Chapter5/RaisedToPower.java
+++ src/Chapter5/RaisedToPower.java
@@ -3,5 +3,11 @@ package Chapter5;
 public class RaisedToPower {
     public static void main(String[] args) {
 
+
+        double value;
+
+        value = Math.pow(2.5,3);
+
+        System.out.printf("The value is " +value);
     }
 }
