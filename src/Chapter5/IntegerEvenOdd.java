1c420f0b3181bd4c518fb729908f6cd1d370c1f4
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2023-12-12 15:47:38 +0530

    Your Message

--- src/Chapter5/IntegerEvenOdd.java
+++ src/Chapter5/IntegerEvenOdd.java
@@ -1,6 +1,6 @@
 package Chapter5;
 import java.util.Scanner;
-public class FindErrors {
+public class IntegerEvenOdd {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
--- src/Chapter5/SumOdd.java
+++ src/Chapter5/SumOdd.java
@@ -4,8 +4,7 @@ public class SumOdd {
     public static void main(String[] args) {
 
         int sum = 0;
-
-        for (int counter = 1; counter <= 99; counter +=3) {
+        for (int counter = 1; counter <= 99; counter +=2) {
 
             sum += counter;
 
