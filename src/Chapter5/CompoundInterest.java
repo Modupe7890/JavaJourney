e5591295fde4eb5678e7cc45b78726707230c75a
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-01-11 11:37:52 +0530

    Your message

--- src/Chapter5/CompoundInterest.java
+++ src/Chapter5/CompoundInterest.java
@@ -2,26 +2,24 @@ package Chapter5;
 import java.util.Scanner;
 public class CompoundInterest {
     public static void main(String[] args) {
-        int amount;
         int principal;
         double rate;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the Amount: ");
-        amount = input.nextInt();
         System.out.println("Enter the Principal amount: ");
         principal = input.nextInt();
         System.out.println("Enter the rate: ");
         rate = input.nextInt();
 
-        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
-        System.out.printf("The compound interest after %d years is: $%.2f\n", years, compoundInterest);
-        System.out.printf("The total amount after %d years is: $%.2f\n", years, principal + compoundInterest);
+        System.out.printf("%s%20s%n", "Year", "Principal Amount");
+
         for (int year = 1; year <= 10; ++year){
 
+        double calculateCompoundInterest = principal * Math.pow(1 + rate, year) - principal;
 
-            System.out.printf("%4d%,20.2f%n", year, amount);
+
+            System.out.printf("%4d%,20.2f%n", year, calculateCompoundInterest);
 
         }
 
