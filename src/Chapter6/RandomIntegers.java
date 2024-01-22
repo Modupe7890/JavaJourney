9165e2656f63af4eb97cce0862d7c111187b7043
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-01-22 13:04:46 +0530

    Your message

--- src/Chapter6/RandomIntegers.java
+++ src/Chapter6/RandomIntegers.java
@@ -6,7 +6,13 @@ public class RandomIntegers {
         SecureRandom randomNumbers = new SecureRandom();
 
         for (int counter = 1; counter <= 20; counter++){
-            int face = 1 +
+            int face = 1 + randomNumbers.nextInt(6);
+
+            System.out.printf("%d ", face);
+
+
+            if (counter % 5 == 0)
+                 System.out.println();
         }
     }
 }
