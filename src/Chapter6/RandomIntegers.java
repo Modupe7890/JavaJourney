983016177118a6f4e298b71706ab6296474e1bef
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-01-22 12:50:26 +0530

    Your message

--- src/Chapter6/RandomIntegers.java
+++ src/Chapter6/RandomIntegers.java
@@ -1,4 +1,12 @@
 package Chapter6;
-
+import java.security.SecureRandom;
 public class RandomIntegers {
+    public static void main(String[] args) {
+
+        SecureRandom randomNumbers = new SecureRandom();
+
+        for (int counter = 1; counter <= 20; counter++){
+            int face = 1 +
+        }
+    }
 }
