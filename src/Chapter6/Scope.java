b70cafca314e2b6f829e9393a56efeb03156125c
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-01-25 14:28:01 +0530

    Your message

--- src/Chapter6/Scope.java
+++ src/Chapter6/Scope.java
@@ -5,5 +5,10 @@ public class Scope {
 
     public static void main(String[] args) {
         int x = 5;
+        System.out.printf("local x in main is %d%n", x);
+    }
+    public static void useLocalVariable(){
+        int x = 25;
+        System.out.printf("%nlocal x on entering method useLocalVariable is %d%n", x);
     }
 }
