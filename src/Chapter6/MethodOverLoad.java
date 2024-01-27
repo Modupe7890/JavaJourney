1e38460a6677c9450ab5d6e4bff5022ba97d0b46
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-01-27 21:27:21 +0530

    Your message

--- src/Chapter6/MethodOverLoad.java
+++ src/Chapter6/MethodOverLoad.java
@@ -2,6 +2,7 @@ package Chapter6;
 
 public class MethodOverLoad {
     public static void main(String[] args) {
-
+        System.out.printf("Square of integer 7 is %d%n", square(7));
+        System.out.printf("Square of double 7.5 is %f%n", sqaure(7.5));
     }
 }
