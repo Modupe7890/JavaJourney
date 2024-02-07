3dd756d04cec7de0eb1547032cf54fe1739f1424
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-07 11:46:55 +0530

    Your message

--- src/Chapter6/MathTest.java
+++ src/Chapter6/MathTest.java
@@ -3,10 +3,12 @@ public class MathTest {
 public static void main(String[] args) {
     public static void
     private int localVariable(0);
+    int value
        System.out.println("");
        System.out.println(" ttttthe");
     System.out.println("the");
     System.out.println(" print the correct answer");
+    System.out.println("the value is ");
 
   }
 }
