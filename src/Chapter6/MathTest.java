5dcca8db7629102ca86e3f18364d8edc5f217d2d
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-07 11:45:11 +0530

    Your message

--- src/Chapter6/MathTest.java
+++ src/Chapter6/MathTest.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
        System.out.println("");
        System.out.println(" ttttthe");
     System.out.println("the");
+    System.out.println(" print the correct answer");
 
   }
 }
