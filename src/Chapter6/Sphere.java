d26d0f3170f52b860a49f6815255a0f0629dfe1a
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-13 14:43:32 +0530

    Your message

--- src/Chapter6/Sphere.java
+++ src/Chapter6/Sphere.java
@@ -7,5 +7,11 @@ public class Sphere {
 
         System.out.println("Enter the radius of the Sphere");
         double radius = input.nextDouble();
+
+        System.out.printf("Volume is %f%n", sphereVolume(radius));
+    }
+    public static double sphereVolume(double radius){
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+        return volume;
     }
 }
