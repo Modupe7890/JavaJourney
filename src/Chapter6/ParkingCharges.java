8dde1ff6eb252a165c9b47eab2ead790ec8a64d6
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-14 14:45:44 +0530

    Your message

--- src/Chapter6/ParkingCharges.java
+++ src/Chapter6/ParkingCharges.java
@@ -1,4 +1,9 @@
 package Chapter6;
-
+import java.util.Scanner;
 public class ParkingCharges {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+    }
 }
