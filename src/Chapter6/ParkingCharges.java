f227ad7a6fb170fcb35ada7f6dda55f30056778e
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-17 21:06:25 +0530

    Your message

--- src/Chapter6/ParkingCharges.java
+++ src/Chapter6/ParkingCharges.java
@@ -42,5 +42,6 @@ public class ParkingCharges {
         totalCharges += charge;
         System.out.printf("%nRunning total of yesterday's receipts is: %.2f", calculculateCharge());
         totalCharges += charge;
+        totalCharges += charge;
     }
 }
