2065d1526142d2168e0e8442a6e243fc59f4ba33
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-22 23:15:46 +0530

    Your message

--- src/Chapter6/ParkingCharges.java
+++ src/Chapter6/ParkingCharges.java
@@ -36,6 +36,7 @@ public class ParkingCharges {
             System.out.printf("Customer charge is: %.2f%n", calculateCharges(hoursParked));
             totalCharges += calculateCharges(hoursParked);
             totalCharges += calculateCharges(hoursParked);
+            totalCharges += calculateCharges(hoursParked);
         }
 
     }
