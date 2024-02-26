cd058236cdaa09ad0f39c58149e560e8feee8082
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-26 16:24:14 +0530

    Your message

---
+++ src/Chapter6/CircleArea.java
@@ -0,0 +1,10 @@
+package Chapter6;
+import java.util.Scanner;
+public class CircleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter value for radius of a circle: ");
+        double radius = input.nextDouble();
+    }
+}
