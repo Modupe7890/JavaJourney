227b7199e3a9704150784118b611e0969f44b165
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-27 22:04:57 +0530

    Your message

--- src/Chapter6/CircleArea.java
+++ src/Chapter6/CircleArea.java
@@ -4,7 +4,18 @@ public class CircleArea {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+
         System.out.println("Enter value for radius of a circle: ");
         double radius = input.nextDouble();
+
+        double area = circleArea(radius);
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+
+    }
+    public static double circleArea(double radius){
+
+        double area = Math.PI * Math.pow(radius, 2);
+        return area;
+
     }
 }
