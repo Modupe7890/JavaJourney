738cdf5fa3c1202262385af121e7f7106ee10509
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-02-29 18:05:54 +0530

    Your message

---
+++ src/Chapter6/FindMinNum.java
@@ -0,0 +1,16 @@
+package Chapter6;
+import java.util.Scanner;
+public class FindMinNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        double num1 = input.nextDouble();
+        System.out.println("enter the second number: ");
+        double num2 = input.nextDouble();
+        System.out.println("Enter the third number: ");
+        double num3 = input.nextDouble();
+
+
+    }
+}
