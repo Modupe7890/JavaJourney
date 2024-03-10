bbe2a451f9e72d36641ae485e4af748477e6895d
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-03-10 14:55:53 +0530

    Your message

--- src/Chapter7/RollDie.java
+++ src/Chapter7/RollDie.java
@@ -13,6 +13,9 @@ public class RollDie {
 
             for (int face = 1; face < frequency.length; face++) {
                 System.out.printf("%4d%10d%n", face, frequency[face]);
+                System.out.printf("%4d%10d%n", face, frequency[face]);
+                System.out.printf("%4d%10d%n", face, frequency[face]);
+
 
             }
         }
