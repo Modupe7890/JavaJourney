70dcb6dc715b56d974b386029d8264e091d758d3
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-03-14 20:54:19 +0530

    Your message

--- src/Chapter7/StudentPoll.java
+++ src/Chapter7/StudentPoll.java
@@ -10,7 +10,7 @@ public class StudentPoll {
                 ++frequency[responses[answer]];
                 }
             catch(ArrayIndexOutOfBoundsException e){
-                System.out.println(e);
+               
                 System.out.printf("response [%d] = %d%n%n", answer, responses[answer]);
 
             }
@@ -18,6 +18,7 @@ public class StudentPoll {
         System.out.printf("%s%10s%n", "Rating", "Frequency");
 
         for(int rating =1; rating < frequency.length; rating++) {
+            System.out.printf("%6d%10d%n", rating, frequency[rating]);
             System.out.printf("%6d%10d%n", rating, frequency[rating]);
         }
 
