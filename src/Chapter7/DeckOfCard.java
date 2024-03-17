c7835eb06778318b49bbbe1ac24a30b5234077c0
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-03-17 18:06:14 +0530

    Your message

--- src/Chapter7/DeckOfCard.java
+++ src/Chapter7/DeckOfCard.java
@@ -3,4 +3,12 @@ import java.security.SecureRandom;
 public class DeckOfCard {
     private Card[] deck;
     private int currentCard;
+    private static final int NUMBER_OF_CARDS = 52;
+    private static final  SecureRandom randomNumbers = new SecureRandom();
+
+    // Constructor fills deck of Cards
+    public DeckOfCard(){
+        String[] faces = {"Ace", "Deuce", "Three", "Four", "Five", "Six",
+                "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+    }
 }
