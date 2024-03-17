c303f5c90b8d247f0b4f1d8da6f5839b01cc2c71
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-03-17 17:35:18 +0530

    Your message

---
+++ src/Chapter7/Cards.java
@@ -0,0 +1,12 @@
+package Chapter7;
+
+public class Cards {
+    private final String face;
+    private final String suit;
+
+    // initializing constructor
+    public class (String cardFace, String cardSuit){
+        this.face = cardFace;
+        this.suit = cardSuit;
+    }
+}
