78cbf3863cc242bcee1952b3a5731415cb0f7361
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-03-23 21:15:44 +0530

    Your message

--- src/Chapter7/EnhancedForTest.java
+++ src/Chapter7/EnhancedForTest.java
@@ -12,6 +12,7 @@ public class EnhancedForTest {
             System.out.printf("Total of array elements: %d%n", total);
             System.out.printf("Total of array elements: %d%n", total);
             System.out.printf("Total of array elements: %d%n", total);
+            System.out.printf("Total of array elements: %d%n", total);
 
 
 
