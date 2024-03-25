cf69d55c3933958e6d20def7249214eae71e01bc
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-03-25 17:44:09 +0530

    your Message

--- src/Chapter7/PassArray.java
+++ src/Chapter7/PassArray.java
@@ -6,8 +6,27 @@ public class PassArray {
 
         System.out.printf("Effects of passing reference to entire array:%n" +
                 "The values of the original array are:%n");
-        for (int value ; array){
+        for (int value : array){
             System.out.printf(" %d", value);
         }
+        for (int value : array) {
+            System.out.printf(" %d", value);
+
+            System.out.printf("%n%nEffects of passing array element value:%n" +
+                    "array[3] before modifyElement: %d%n", array[3]);
+
+            modifyElement(array[3]);
+            System.out.printf(
+                    "array[3] after modifyElement: %d%n", array[3]);
+        }
+    }
+    public static void modifyArray(int[] array2){
+        for(int counter = 0; counter < array2.length; counter++){
+            array2[counter] *= 2;
+        }
+    }
+    public static void modifyElement(int element){
+        element *= 2;
+        System.out.printf("Value of element in modifyElement: %d%n", element);
     }
 }
