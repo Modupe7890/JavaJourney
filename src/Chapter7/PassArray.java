f899f69a15a347f20e8ea5b94560936b332ad1cb
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-03-25 17:01:03 +0530

    Your message

---
+++ src/Chapter7/PassArray.java
@@ -0,0 +1,10 @@
+package Chapter7;
+
+public class PassArray {
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4,5};
+
+        System.out.printf("Effects of passing reference to entire array:%n" +
+                "The values of the original array are:%n");
+    }
+}
