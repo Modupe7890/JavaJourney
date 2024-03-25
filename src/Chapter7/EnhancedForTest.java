a8d8f007453cf6b0cf221baa12c2151cff9f8603
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-03-25 12:35:53 +0530

    Your message

--- src/Chapter7/EnhancedForTest.java
+++ src/Chapter7/EnhancedForTest.java
@@ -5,9 +5,11 @@ public class EnhancedForTest {
         int [] array = {87,68,94,100,83,78,85,91,76,87};
         int total = 0;
 
-        for (int number = 0; number < array.length; number++){
-            total += number;
+        for (int number = 0; number < array.length; number++) {
+            total += array[number];
+
             System.out.printf("Total of array elements: %d%n", total);
         }
+
     }
 }
