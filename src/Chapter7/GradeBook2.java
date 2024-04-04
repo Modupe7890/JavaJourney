738399e761f6abc8a4c9047621966defaf06834c
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-04 15:38:31 +0530

    Your message

--- src/Chapter7/GradeBook2.java
+++ src/Chapter7/GradeBook2.java
@@ -1,4 +1,14 @@
 package Chapter7;
 
 public class GradeBook2 {
+    private String courseName;
+    private int[][] grades;
+    public GradeBook2(String courseName, int[][] grades){
+        this.courseName = courseName;
+        this.grades = grades;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
 }
