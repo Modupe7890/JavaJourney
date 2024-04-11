dba9772eb37231991c65d09538e60873b89f4fcf
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-11 18:36:00 +0530

    Your message

--- src/Chapter7/ArrayListCollection.java
+++ src/Chapter7/ArrayListCollection.java
@@ -8,7 +8,7 @@ public class ArrayListCollection {
         items.add(0, "yellow");
         System.out.print("Display list contents with counter-controlled loop: ");
 
-        for(int i = 0; i < items.size(); i++) {
+        for (int i = 0; i < items.size(); i++) {
             System.out.printf("%s ", items.get(i));
             display(items, "%nDisplay list contents with enhanced for statement: ");
 
@@ -17,27 +17,25 @@ public class ArrayListCollection {
             display(items, "List with two new elements: ");
 
             items.remove("yellow");
-            display(items,"Remove first instance of yellow: ");
+            display(items, "Remove first instance of yellow: ");
+
 
             items.remove(1);
             display(items, "Remove second list element(green): ");
 
             //check if value is in list
-            System.out.printf("\"red\" is %sin the list%n", items.contains("red") ? "": "not ");
+            System.out.printf("\"red\" is %sin the list%n", items.contains("red") ? "" : "not ");
+
 
             System.out.printf("Size: %s%n", items.size());
-
+        }
+    }
             //Display Arraylist elements on the console
             public static void display(ArrayList<String> items, String header){
                 System.out.printf(header);
 
-                for(String items : items){
-                    System.out.printf(" %s", item);
-                    System.out.printf(" %s", item);
-                    System.out.printf(" %s", item);
+                for(String item : items){
                     System.out.printf(" %s", item);
                 }
             }
-        }
-    }
 }
