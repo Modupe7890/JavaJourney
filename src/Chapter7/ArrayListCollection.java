de789cd92659a43e556a1991c5d2124569ed860e
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-11 18:26:10 +0530

    Your message

--- src/Chapter7/ArrayListCollection.java
+++ src/Chapter7/ArrayListCollection.java
@@ -10,6 +10,31 @@ public class ArrayListCollection {
 
         for(int i = 0; i < items.size(); i++) {
             System.out.printf("%s ", items.get(i));
+            display(items, "%nDisplay list contents with enhanced for statement: ");
+
+            items.add("green");
+            items.add("yellow");
+            display(items, "List with two new elements: ");
+
+            items.remove("yellow");
+            display(items,"Remove first instance of yellow: ");
+
+            items.remove(1);
+            display(items, "Remove second list element(green): ");
+
+            //check if value is in list
+            System.out.printf("\"red\" is %sin the list%n", items.contains("red") ? "": "not ");
+
+            System.out.printf("Size: %s%n", items.size());
+
+            //Display Arraylist elements on the console
+            public static void display(ArrayList<String> items, String header){
+                System.out.printf(header);
+
+                for(String items : items){
+                    System.out.printf(" %s", item);
+                }
+            }
         }
     }
 }
