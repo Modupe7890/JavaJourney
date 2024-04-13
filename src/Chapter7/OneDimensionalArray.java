844c568f89d77507811c073d2a07c075ade63c37
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-13 16:34:45 +0530

    your Message

---
+++ src/Chapter7/OneDimensionalArray.java
@@ -0,0 +1,11 @@
+package Chapter7;
+import java.util.Scanner;
+public class OneDimensionalArray {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+
+        System.out.printf("%s%10%s%n", "Index", "Value" );
+
+        for(int counter = 0; counter < numbers.length; counter++);
+    }
+}
