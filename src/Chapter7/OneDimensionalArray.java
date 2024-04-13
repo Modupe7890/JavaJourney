6439ac463e7413793e61d690aea2c482ea61e1e0
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-13 18:08:28 +0530

    your Message

--- src/Chapter7/OneDimensionalArray.java
+++ src/Chapter7/OneDimensionalArray.java
@@ -5,13 +5,20 @@ public class OneDimensionalArray {
 
         // Initializing counts to zero
         int[] counts = new int[10];
+        for (int i = 0; i < counts.length; i++) {
+            counts[i] = 0;
+        }
         // Adding one to each of the 15elements
         int[] number = new int[15];
-        // five values of integer array bestScores
-        int[] bestScores = {70,75,80,85,90};
-        System.out.println("bestScores: ");
-        for (int i = 0; i < bestScores.length; i++){
-            System.out.printf("BestScores", +(i+1) + ":", +bestScores[i]);
+        for (int i = 0; i < number.length; i++) {
+            number[i] = i + 1;
         }
-            }
+        // five values of integer array bestScores
+        int[] bestScores = {70, 75, 80, 85, 90};
+        System.out.printf("%s: ", "bestScores");
+
+        for (int i = 0; i < bestScores.length; i++) {
+            System.out.printf("BestScores", +(i + 1) + ":", +bestScores[i]);
+        }
+    }
 }
