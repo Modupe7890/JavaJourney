dc387d07007775c557934217995fe7d345c273c9
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-16 15:12:36 +0530

    your Message

--- src/Chapter7/DuplicateEliminate.java
+++ src/Chapter7/DuplicateEliminate.java
@@ -1,5 +1,9 @@
 package Chapter7;
-
+import java.util.Scanner;
 public class DuplicateEliminate {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+    }
 
 }
