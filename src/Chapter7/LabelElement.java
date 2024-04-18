c1a20380e2b12d0b2e15f0612fa72ad00edd486c
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-18 20:10:02 +0530

    your Message

--- src/Chapter7/LabelElement.java
+++ src/Chapter7/LabelElement.java
@@ -4,5 +4,13 @@ public class LabelElement {
     public static void main(String[] args) {
 
         int[][] sales = new int[3] [5];
+        for (int row = 0; row < sales.length; row++) {
+            for (int col = 0; col < sales[row].length; col++) {
+                sales[row][col] = 0;
+
+                System.out.printf("row %d column %d = %d%n", row, col, sales[row][col]);
+                System.out.printf("row %d column %d = %d%n", row, col, sales[row][col]);
+            }
+        }
     }
 }
