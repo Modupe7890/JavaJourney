130a57be61f96be67e6dd4f3084add0334ba87b2
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-18 20:11:58 +0530

    your Message

--- src/Chapter7/LabelElement.java
+++ src/Chapter7/LabelElement.java
@@ -12,6 +12,7 @@ public class LabelElement {
                 System.out.printf("row %d column %d = %d%n", row, col, sales[row][col]);
                 System.out.printf("row %d column %d = %d%n", row, col, sales[row][col]);
                 System.out.printf("row %d column %d = %d%n", row, col, sales[row][col]);
+                System.out.printf("row %d column %d = %d%n", row, col, sales[row][col]);
             }
         }
     }
