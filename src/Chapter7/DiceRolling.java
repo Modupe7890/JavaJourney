b1ba950f95df5043bed64f4aee1f130d848b9c38
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-22 15:08:52 +0530

    your Message

--- src/Chapter7/DiceRolling.java
+++ src/Chapter7/DiceRolling.java
@@ -2,6 +2,14 @@ package Chapter7;
 import java.util.Random;
 public class DiceRolling {
     public static void main(String[] args) {
+        int[] sum = new int[11];
+
+        Random random = new Random();
+
+        for(int i = 0; i < 36000000; i++){
+            int die1 =
+        }
+
 
     }
 
