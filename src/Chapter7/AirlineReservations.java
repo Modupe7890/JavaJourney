54096121c8eb71e1b97806c82222a996feffeddd
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-23 11:09:38 +0530

    your Message

--- src/Chapter7/AirlineReservations.java
+++ src/Chapter7/AirlineReservations.java
@@ -1,6 +1,29 @@
 package Chapter7;
 import java.util.Scanner;
 public class AirlineReservations {
-    boolean[] planeSeats = new boolean[10];
+
+    static boolean[] planeSeats = new boolean[10];
     Scanner input = new Scanner(System.in);
+
+    public static void firstClassChoice(){
+        for(int seatNumber = 0; seatNumber < planeSeats.length; seatNumber++){
+            if (!planeSeats[seatNumber]) {
+                System.out.println("Your seat Number is " +seatNumber + "\n First Class");
+                planeSeats[seatNumber] = true;
+                break;
+            }
+        }
+    }
+    public  static void economyChoice(){
+        for(int seatNumber = 5; seatNumber < planeSeats.length; seatNumber++){
+            if (!planeSeats[seatNumber]) {
+                System.out.println("Your seat Number is " +seatNumber + "\n First Class");
+                planeSeats[seatNumber] = true;
+                break;
+            }
+        }
+    }
+    public static void alternativePlacementForFirstClass(){
+
+    }
 }
