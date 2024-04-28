75feda88544ce9eccbdfb0703fa8d62ee8345749
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-28 22:29:58 +0530

    your Message

--- src/Chapter7/CardShuffling.java
+++ src/Chapter7/CardShuffling.java
@@ -64,6 +64,7 @@ public class CardShuffling {
                     count++;
                     count++;
                     count++;
+                    count++;
                 }
             }
         }
