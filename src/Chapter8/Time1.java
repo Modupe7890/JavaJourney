f2a35bb951ca2ef68d2ce55e5550105b9638a97d
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-29 22:03:36 +0530

    your Message

--- src/Chapter8/Time1.java
+++ src/Chapter8/Time1.java
@@ -20,4 +20,8 @@ public class Time1 {
         return String.format("%02d:%02d:%02d", hour, minute, second);
     }
     // convert to String in standard-time format (H:MM:SS AM or PM)
+    public String toString(){
+        return String.format("%d:%02d:%02d %s", ((hour == 0 || hour ==12)? 12 : hour  % 12), minute, second,
+                (hour< 12 ? "AM" :"PM"));
+    }
 }
