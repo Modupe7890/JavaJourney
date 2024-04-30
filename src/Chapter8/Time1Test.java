35ce585445da5cfc62623317573fb5cc8cca4090
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-30 11:21:25 +0530

    your Message

--- src/Chapter8/Time1Test.java
+++ src/Chapter8/Time1Test.java
@@ -27,7 +27,7 @@ public class Time1Test {
     // displays a Time1 object in 24-hour and 12-hour formats
     public static void displayTime(String header, Time1 t){
         System.out.printf("%s%nUniversal time: %s%nStandard time: %s%n",
-                t.toUniversalString(), t.toString());
+                header, t.toUniversalString(), t.toString());
 
     }
 }
