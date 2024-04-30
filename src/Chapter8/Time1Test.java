d872a077688652f753b1b1822a3d831f6b311db8
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-04-30 11:19:06 +0530

    Your message

--- src/Chapter8/Time1Test.java
+++ src/Chapter8/Time1Test.java
@@ -25,7 +25,9 @@ public class Time1Test {
         displayTime("After calling setTime with invalid values", time);
     }
     // displays a Time1 object in 24-hour and 12-hour formats
-    public static void displayTime(){
+    public static void displayTime(String header, Time1 t){
+        System.out.printf("%s%nUniversal time: %s%nStandard time: %s%n",
+                t.toUniversalString(), t.toString());
 
     }
 }
