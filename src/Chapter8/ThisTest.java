363fc63686951e5cc6c746041c99befad69d6a16
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-06 14:56:27 +0530

    your Message

--- src/Chapter8/ThisTest.java
+++ src/Chapter8/ThisTest.java
@@ -12,14 +12,23 @@ class  SimpleTime {
     private int minute;
     private int second;
 
+    // if the constructor uses parameter names identical to
+    // instance variable names the "this" reference is
+    // required to distinguish between the names
     public SimpleTime(int hour, int minute, int second){
         this.hour = hour;
         this.minute = minute;
         this.second = second;
     }
 
+    // use explicit and implicit "this" to call toUniversalString
     public String buildString(){
         return String.format("%24s: %s%n%24s: %s","this.toUniversalString()",
-                this.toUniversalString(), "toUniversalString", toUniversalString);
+                this.toUniversalString(), "toUniversalString", toUniversalString());
+    }
+
+    // convert to String in universal-time format (HH:MM:SS)
+    public String toUniversalString(){
+        return String.format("%02d:%02d:%02d")
     }
 }
