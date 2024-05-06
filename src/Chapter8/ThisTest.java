e3a780bb563e1df0d434ec6774f5c4c75fc62447
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-06 14:24:21 +0530

    your Message

---
+++ src/Chapter8/ThisTest.java
@@ -0,0 +1,19 @@
+package Chapter8;
+
+public class ThisTest {
+    public static void main(String[] args) {
+        SimpleTime time = new SimpleTime(15, 30, 19);
+        System.out.println(time.buildString());
+    }
+}
+class  SimpleTime {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public SimpleTime(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+
+    }
+}
