630c7cdf7b882f1deb9da5c94bb6fd929221e2d7
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-07 20:29:38 +0530

    your Message

--- src/Chapter8/Time2.java
+++ src/Chapter8/Time2.java
@@ -20,6 +20,13 @@ public class Time2 {
         this(hour, minute, 0);
     }
     public Time2(int hour, int minute, int second){
+        if (hour < 0 || hour >= 24)
+            throw new IllegalArgumentException("hour must be 0-23");
 
+        if (minute < 0 || minute >= 60)
+            throw new IllegalArgumentException("minute must be 0-59");
+
+        if (second < 0 || second >= 60)
+            throw new IllegalArgumentException("second must be 0-59");
     }
 }
