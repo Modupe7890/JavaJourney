7920bddb4e70ba9968c186ab7b64e8bb1cc5a6d4
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-08 16:00:46 +0530

    Your message

--- src/Chapter8/Time2.java
+++ src/Chapter8/Time2.java
@@ -109,5 +109,6 @@ public class Time2 {
         getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
         getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
         getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
+        getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
     }
 }
