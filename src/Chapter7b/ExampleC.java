7771fb92cc28a3a3e89b6e3a439f01374a273b87
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-13 14:45:15 +0530

    your Message

--- src/Chapter7b/ExampleC.java
+++ src/Chapter7b/ExampleC.java
@@ -1,14 +1,13 @@
 package Chapter7b;
-
 import java.util.Arrays;
-
-public class ExampleB {
+public class ExampleC {
     public static void main(String[] args) {
-    //10-element array with numbers from 10 to 1
+
+        //10-element array with numbers from 0 to 9
         int[] numbers = new int[10];
 
         for(int i = 0; i < numbers.length; i++){
-            numbers[i] = 10 - i;
+            numbers[i] = i;
         }
         System.out.println(Arrays.toString(numbers));
     }
