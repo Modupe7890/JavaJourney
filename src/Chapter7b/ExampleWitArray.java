5ae1b51591b7e8ccddaaefaf42dde7a0c5c446a7
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-15 14:02:09 +0530

    your Message

---
+++ src/Chapter7b/ExampleWitArray.java
@@ -0,0 +1,16 @@
+package Chapter7b;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class ExampleWitArray {
+    public static void main(String[] args) {
+        Reader r = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(r);
+
+        //Read strings from keyboard
+        String[] list = new String[10];
+    }
+}
