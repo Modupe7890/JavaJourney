e3d7388fa16e9266aafb525e3879025bea9c3bc5
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-17 15:02:18 +0530

    your Message

--- src/Chapter7b/ExampleWitArrayList.java
+++ src/Chapter7b/ExampleWitArrayList.java
@@ -26,6 +26,7 @@ public class ExampleWitArrayList {
             System.out.println( list.get(j) );
             System.out.println( list.get(j) );
             System.out.println( list.get(j) );
+            System.out.println( list.get(j) );
         }
     }
 }
